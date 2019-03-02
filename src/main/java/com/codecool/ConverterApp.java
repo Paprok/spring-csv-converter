e670bcd909b25bfda001245b7e14db6c78795ca6
Paprok <rafal.becla@gmail.com>
2019-03-02 16:43:39 +0100

    implemented command line arguments

--- src/main/java/com/codecool/ConverterApp.java
+++ src/main/java/com/codecool/ConverterApp.java
@@ -1,9 +1,30 @@
 package com.codecool;
 
 
-public class App
-{
-    public static void main( String[] args )
-    {
+import com.codecool.view.View;
+import com.codecool.view.ViewTerm;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class ConverterApp {
+    public static void main(String[] args) {
+        View view = new ViewTerm();
+        Formats format = Formats.TABLE;
+        FileReader file;
+        try {
+            if (args.length < 1) {
+                view.noInputFileMsg();
+            } else if (args.length == 1) {
+                file = new FileReader(args[0]);
+            } else {
+                format = Formats.getFormat(args[0]);
+                file = new FileReader(args[1]);
+            }
+        } catch (FormatNotFoundException e) {
+            view.wrongFormatMsg();
+        } catch (FileNotFoundException e) {
+            view.fileNotFound();
+        }
     }
 }
--- src/test/java/com/codecool/ConverterAppTest.java
+++ src/test/java/com/codecool/ConverterAppTest.java
@@ -5,9 +5,9 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 /**
- * Unit test for simple App.
+ * Unit test for simple ConverterApp.
  */
-public class AppTest 
+public class ConverterAppTest
 {
     /**
      * Rigorous Test :-)
