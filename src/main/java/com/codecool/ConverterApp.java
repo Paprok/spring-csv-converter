24083e4b57a1fa6b4b77dbc807819f6cdfebed79
Paprok <rafal.becla@gmail.com>
2019-03-02 18:33:29 +0100

    implemented dealing with different command line args

--- src/main/java/com/codecool/ConverterApp.java
+++ src/main/java/com/codecool/ConverterApp.java
@@ -1,25 +1,26 @@
 package com.codecool;
 
 
-import com.codecool.view.View;
-import com.codecool.view.ViewTerm;
-
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 
 public class ConverterApp {
     public static void main(String[] args) {
         View view = new ViewTerm();
-        Formats format = Formats.TABLE;
+        Formats format;
         FileReader file;
+        FileLoader loader = new FileLoader();
+        SimpleCsvConverter converter = new SimpleCsvConverter();
         try {
             if (args.length < 1) {
                 view.noInputFileMsg();
             } else if (args.length == 1) {
-                file = new FileReader(args[0]);
+                file = loader.loadFile(args[0]);
+                converter.converter(file);
             } else {
                 format = Formats.getFormat(args[0]);
-                file = new FileReader(args[1]);
+                file = loader.loadFile(args[1]);
+                converter.converter(file, format);
             }
         } catch (FormatNotFoundException e) {
             view.wrongFormatMsg();
