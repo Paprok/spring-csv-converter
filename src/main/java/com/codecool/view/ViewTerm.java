3461769a28e260e181d631eebe2c015fa32a7d93
Paprok <rafal.becla@gmail.com>
2019-03-02 16:45:14 +0100

    created terminal view implementation

---
+++ src/main/java/com/codecool/view/ViewTerm.java
@@ -0,0 +1,18 @@
+package com.codecool.view;
+
+public class ViewTerm implements View {
+    @Override
+    public void noInputFileMsg() {
+        System.out.println("No input file defined");
+    }
+
+    @Override
+    public void wrongFormatMsg() {
+        System.out.println("Format not found");
+    }
+
+    @Override
+    public void fileNotFound() {
+        System.out.println("File not found");
+    }
+}
