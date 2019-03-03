b596dc43daf22ae84421be0602d026a9845320ac
Paprok <rafal.becla@gmail.com>
2019-03-03 15:56:25 +0100

    implemented OutputFormatter interface

--- src/main/java/com/codecool/scc/formatters/XmlTermFormatter.java
+++ src/main/java/com/codecool/scc/formatters/XmlTermFormatter.java
@@ -1,4 +1,11 @@
 package com.codecool.scc.formatters;
 
-public class XmlTermFormatter {
+import java.util.List;
+
+public class XmlTermFormatter implements OutputFormatter {
+
+    @Override
+    public void showFormatted(List<String> list) {
+        System.out.println("totally xml formatted");
+    }
 }
